41a7025a09518da8c4a81ab0821ecd5f070988ea
Andrew Benson <andebenson@gmail.com>
2012-11-07 15:55:33 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Windmill::Application.config.secret_token = '581a7a11d11fc2854869ec05a43301baf46656d586fdce50ea4364652329fc32fdacd3dbc3d7c42a1aba99c2cdd308efb7da98a191fba6b9e175f27223646465'
